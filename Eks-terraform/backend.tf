3e804e825098b7552926b2a3a219a88662433611
binank <50366552+binank@users.noreply.github.com>
2024-04-23 08:12:52 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "binank19871" # Replace with your actual S3 bucket name
+    bucket = "skyvera" # Replace with your actual S3 bucket name
+    key="terraform.tfstate"
     region = "ap-south-1"
   }
 }
